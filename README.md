66d2b2e48139aa1e2a6399bd58cde93b5febea2e
Luis <m.luishernandez@outlook.com>
2021-07-24 20:57:57 -0400

    Working properly, need to change some variable names and add/clean how readme is generated

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
-# It is what it is
+# README-Generator
 
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
   ## Description
-  It is a project design to do what I want it to do
+  This is a simple README Generator using inquirer npm and javascript
 
   ## Installation
   ```
-  Just donwload and npm i
+  npm i
   ```
 
+  ## Usage
+  ```
+  node index.js
+  ```
+
+  ## Test
+  ```
+  node index.js
+  ```
+
+  ## Contact
+
+  If you have any questions about this repository, open an issue or contact me directly at m.luishernandez@outlook.com. 
+  
+  You can find more of my work at https://github.com/LuisM-Hernandez
+
+  ## License
+
+    This project is licensed under the MIT license.
+
+    
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,22 +1,75 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 const backticks = "```"
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
+function getLicenseBadge(license) {
 
+  if(license === "MIT") return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
+  
+  else if(license === "APACHE 2.0") return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+ 
+  else if(license === "GPL 3.0") return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+ 
+  else if(license === "BSD 3") return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]";
+   
+ else if(license === "NONE")  return "";
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function getLicenseLink(license) {
+  if(license === "MIT") return "(https://opensource.org/licenses/MIT)";
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  else if(license === "APACHE 2.0") return "(https://opensource.org/licenses/Apache-2.0)";
+   
+  else if(license === "GPL 3.0") return "(https://www.gnu.org/licenses/gpl-3.0)";
+ 
+  else if(license === "BSD 3") return "(https://opensource.org/licenses/BSD-3-Clause)";
+ 
+  else if(license === "NONE") return "";
+}
 
-// TODO: Create a function to generate markdown for README
+function getLicenseSection(license) {
+  if(license === "MIT"){
+    return `## License
+
+    This project is licensed under the MIT license.
+
+    `
+  }
+
+ else if(license === "APACHE 2.0"){
+  return `## License
+
+  This project is licensed under the Apache license.
+
+  `
+  }
+  else if(license === "GPL 3.0"){
+    return `## License
+
+    This project is licensed under the GPL license.
+
+
+    `
+  }
+ else if(license === "BSD 3"){
+  return `## License
+
+  This project is licensed under the BSD license.
+
+  `
+  }
+
+  else if (license === "NONE"){
+    return `## License
+    
+    `
+  }
+}
+
 function generateMarkdown(data) {
+  const badge = getLicenseBadge(data.license);
+  const links = getLicenseLink(data.license)
+  const section = getLicenseSection(data.license);
   return `# ${data.title}
 
+  ${badge}${links}
   ## Description
   ${data.description}
 
@@ -33,6 +86,13 @@ function generateMarkdown(data) {
   ${data.test}
   ${backticks}
 
+  ## Contact
+
+  If you have any questions about this repository, open an issue or contact me directly at ${data.email}. 
+  
+  You can find more of my work at https://github.com/${data.github}
+
+  ${section}
 
 `;
 }
